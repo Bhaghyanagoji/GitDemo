b54e91506eb65d17e801a48f1273627da9cc4cc7
Bhaghyanagoji <bhaghyanagoji@gmail.com>
2023-05-26 10:46:34 +0530

    added

---
+++ Hello.java
@@ -0,0 +1,22 @@
+class Hello{
+    public static void main(String[] args)
+    {
+        int a=6;
+        System.out.println(a);
+
+        int b=020;
+        System.out.println(b);
+
+        int c=0b111;
+        System.out.println(c);
+
+        int d=0xAB112;
+        System.out.println(d);
+        
+        int e=10_000;
+        System.out.println(e);
+        
+        double f=12e12;
+        System.out.println(f);
+    }
+}
